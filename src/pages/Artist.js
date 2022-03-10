2472a1fd3e8ce125e10c72215dec06bed302262e
najeesimmons <najee.bryant.simmons@gmail.com>
2022-03-10 00:02:10 -0500

    added links to Artist page

--- src/pages/Artist.js
+++ src/pages/Artist.js
@@ -1,14 +1,15 @@
-
+import { Link } from "react-router-dom";
 
 const Artist = (props) => {
 
     return (
       <div>
-        {props.artists.map((artist, index) => {
+        {props.artists.map((artist) => {
           return (
             <div key={artist._id}>
+              <Link to={`/artist/${artist._id}`} >
               <h1>{artist.first_name}</h1>
-              <h2>{index}</h2>
+              </Link>
             </div>
           )
         })}
