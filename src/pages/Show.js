0469a92589ec6f15235a2172f448d0af4c1fb496
najeesimmons <najee.bryant.simmons@gmail.com>
2022-03-09 23:38:17 -0500

    factored Show page/ functional

--- src/pages/Show.js
+++ src/pages/Show.js
@@ -1,12 +1,19 @@
-import { useState, useEffect } from "react";
 import { useParams } from "react-router";
 
 const Show = (props) => {
 
     const params = useParams()
-    const id = params._id;
 
-
+    const artist = props.artists.find(artist => artist._id === params.id)
+    console.log(artist)
+    if (artist) {
+        return (
+            <div>
+               {artist.password}
+            </div>
+        )
+    }
+    return null
 };
 
 export default Show;
