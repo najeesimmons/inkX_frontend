24b521d9353214cf2807435b7ed66ee00e78a0c4
najeesimmons <najee.bryant.simmons@gmail.com>
2022-03-10 15:59:38 -0500

    piece index page is funcitonal

--- src/pages/Piece.js
+++ src/pages/Piece.js
@@ -1,11 +1,24 @@
-
+import { Link } from "react-router-dom";
 
 const Piece = (props) => {
-    return (  
+
+    return (
       <div>
-        <h1>This is the Piece Index Page</h1>
+        {props.pieces.map((piece) => {
+          return (
+            <div key={piece._id}>
+              {/* <Link to={`/artist/${artist._id}`} > */}
+              <img src={piece.image} alt={piece.artist} />
+              <p>{piece.date}</p>
+              <p>{piece.artist}</p>
+              <p>{piece.description}</p>
+              <br></br>
+              {/* </Link> */}
+            </div>
+          )
+        })}
       </div>
     )
-  };
+};
   
   export default Piece;
