e02c49b6071baf21caf62535158ea5ecd5e191f8
najeesimmons <najee.bryant.simmons@gmail.com>
2022-03-10 14:45:42 -0500

    added temporary jsx to piece component

---
+++ src/pages/Piece.js
@@ -0,0 +1,11 @@
+import Nav from "../components/Nav";
+
+const Piece = (props) => {
+    return (
+      <div>
+        <h1>This is the Piece Index Page</h1>
+      </div>
+    )
+  };
+  
+  export default Piece;
