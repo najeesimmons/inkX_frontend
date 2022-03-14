6f78013156638b92e31a2e4c0fbe0433bfdcc642
najeesimmons <najee.bryant.simmons@gmail.com>
2022-03-14 14:49:13 -0400

    after failing to loop thru artist pieces in artist show page, I will instead create an ArtistPieces component.

--- src/pages/ArtistShow.js
+++ src/pages/ArtistShow.js
@@ -5,15 +5,19 @@ const Show = (props) => {
     const params = useParams()
 
     const artist = props.artists.find(artist => artist._id === params.id)
+
+    // const pieces = props.pieces.find(piece => piece.artist === params.id)
+
     console.log(artist)
+
     if (artist) {
         return (
             <div>
                 <div>
-                <h1>{artist.first_name}</ h1>
-                <img src={artist.profile_pic} alt="profile"/>
+                    <h1>{artist.first_name}</ h1>
+                    <img src={artist.profile_pic} alt="profile"/>
                 </div>
-                
+                <br></br>
                 <div>
                     
                 </div>
