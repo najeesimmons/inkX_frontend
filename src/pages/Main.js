ef4c0877de1669b0db2c730477f41c52d3ee8393
najeesimmons <najee.bryant.simmons@gmail.com>
2022-03-14 21:41:12 -0400

    added pieces loop to main page

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,4 +1,5 @@
 import NewArtistForm from "../components/NewArtistForm";
+import { Link } from "react-router-dom";
 // import ArtistPieces from "..components/ArtistPieces"
 
 
@@ -10,10 +11,21 @@ const Main = (props) => {
         <img src="https://media.istockphoto.com/vectors/crossed-tattoo-machines-isolated-on-white-background-design-element-vector-id934818866?k=20&m=934818866&s=612x612&w=0&h=FlUG1lKnzyZTqnvWQypV9K_0b1RmBzVkpkHvAdOU-eQ=" alt="inkX logo"/>
         <br></br>
         <NewArtistForm className= "form" createArtist={props.createArtist} />
-        <div>
-        {/* <ArtistPieces></ArtistPieces> */}
-        </div>  
+        <div className="image-container">
+        {props.pieces.map((piece) => {
+          return (
+            <div key={piece._id}>
+              <br></br>
+              <Link to={`/piece/${piece._id}`} >
+                <img src ={piece.image} className="piece-image" alt={piece.title} />
+              </Link>
+              <br></br>
+            </div>
+          )
+        })}
+      </div>  
     </div>
+    
 
   )
 };
