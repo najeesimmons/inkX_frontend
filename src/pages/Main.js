847a8ba6e8f23eeb3d751f0986484f9d1feb994b
najeesimmons <najee.bryant.simmons@gmail.com>
2022-03-16 10:47:35 -0400

    last min style changes

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -15,7 +15,7 @@ const Main = (props) => {
         <div className="form-and-pieces-container">
           <NewArtistForm className= "form" createArtist={props.createArtist} />
           <div className="pieces-container">
-          <h1>New Posts</h1>
+          <h2>Newest Posts</h2>
           {props.pieces.map((piece) => {
           return (
             <div key={piece._id} className="each-piece">
