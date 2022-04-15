8748de098f358f9357eb0d0c63304d1a2bfb0853
najeesimmons <najee.bryant.simmons@gmail.com>
2022-04-15 13:07:04 -0400

    formatting

--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -1,38 +1,39 @@
 import NewArtistForm from "../../components/NewArtistForm/NewArtistForm";
 import { Link } from "react-router-dom";
-import "./main.css"
+import "./main.css";
 import Slideshow from "../../components/SlideShow/Slideshow";
 // import ArtistPieces from "..components/ArtistPieces"
-
 
 const Main = (props) => {
   return (
     <div className="main-container">
-        <h1>ink X</h1>
-        <h2 className="site-title">where artist connect to clients...</h2>
-        <br></br>
-        <Slideshow/>
-        <div className="form-and-pieces-container">
-          <NewArtistForm className= "form" createArtist={props.createArtist} />
-          <div className="pieces-container">
+      <h1>ink X</h1>
+      <h2 className="site-title">where artist connect to clients...</h2>
+      <br></br>
+      <Slideshow />
+      <div className="form-and-pieces-container">
+        <NewArtistForm className="form" createArtist={props.createArtist} />
+        <div className="pieces-container">
           <h2 className="page-title">New Posts</h2>
           {props.pieces.map((piece) => {
-          return (
-            <div key={piece._id} className="each-piece">
-              <br></br>
-              <Link to={`/piece/${piece._id}`} >
-                <img src ={piece.image} className="piece-image" alt={piece.title} />
-              </Link>
-              <br></br>
-            </div>
-          )
-        })}
+            return (
+              <div key={piece._id} className="each-piece">
+                <br></br>
+                <Link to={`/piece/${piece._id}`}>
+                  <img
+                    src={piece.image}
+                    className="piece-image"
+                    alt={piece.title}
+                  />
+                </Link>
+                <br></br>
+              </div>
+            );
+          })}
         </div>
-      </div>  
+      </div>
     </div>
-    
-
-  )
+  );
 };
 
 export default Main;
