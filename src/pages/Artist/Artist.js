1236f787ac5a2f8aed4cd4a7b86b59b94e969f36
najeesimmons <najee.bryant.simmons@gmail.com>
2022-07-16 16:34:55 -0400

    adjusted styles to use cards for artists page/ positioned logo on nav bar

--- src/pages/Artist/Artist.js
+++ src/pages/Artist/Artist.js
@@ -6,19 +6,17 @@ const Artist = (props) => {
     <div className="artist-container">
       {props.artists.map((artist) => {
         return (
-          <div key={artist._id}>
-            <br />
+          <div key={artist._id} className="artist-card">
             <Link to={`/artist/${artist._id}`}>
-              <img
-                src={artist.profile_pic}
-                className="artist_pic"
-                alt={artist.username}
-              />
-              <h3 className="artist-name">{artist.username}</h3>
+            <img src={artist.profile_pic} alt={artist.username} className="artist_pic" />
             </Link>
-            <h3 className="artist-location">
-              {artist.city}, {artist.state}
-            </h3>
+            <div className="artist-info-container">
+              <h4>
+                <b>{artist.username}</b>
+                <p className="artist-location">{artist.city}, {artist.state}</p>
+              </h4>
+            </div>
+          
           </div>
         );
       })}
@@ -27,3 +25,17 @@ const Artist = (props) => {
 };
 
 export default Artist;
+// <div key={artist._id}>
+//   <br />
+//   <Link to={`/artist/${artist._id}`}>
+//     <img
+//       src={artist.profile_pic}
+//       className="artist_pic"
+//       alt={artist.username}
+//     />
+//     <h3 className="artist-name">{artist.username}</h3>
+//   </Link>
+//   <h3 className="artist-location">
+//     {artist.city}, {artist.state}
+//   </h3>
+// </div>
