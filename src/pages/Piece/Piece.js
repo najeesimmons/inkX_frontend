987b11b56744fc71ec13813af93b89fb9244d747
najeesimmons <najee.bryant.simmons@gmail.com>
2022-07-16 18:27:02 -0400

    react  masonry working and rendering in Pieces component/ need to add info card which appears on hover

--- src/pages/Piece/Piece.js
+++ src/pages/Piece/Piece.js
@@ -1,36 +1,54 @@
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 import "./piece.css";
+import Masonry from "react-masonry-css";
 
-const Piece = (props) => {
+const Piece = ({ pieces }) => {
+  const allPieces = pieces.map((piece) => {
+    return (
+      <div key={piece._id}>
+        <img src={piece.image} alt={piece.title} className="piece-pic" />
+      </div>
+    );
+  });
+
+  const breakpointColumnsObj = {
+    default: 4,
+    1100: 3,
+    700: 2,
+    500: 1,
+  };
+
   return (
     <div className="pieces-wrapper">
-    <h2 className="pieces-heading">Explore Tatoos</h2>
-    <div className="pieces-container">
-      {props.pieces.map((piece) => {
-        return (
-          <div key={piece._id} className="piece-card">
-            <Link to={`/piece/${piece._id}`}>
-              <img src={piece.image} alt={piece.title} className="piece-pic" />
-            </Link>
-            {/* <div className="piece-info-container">
-              <h4>
-                <b></b>
-                <p className="artist-location"></p>
-
-              </h4>
-            </div> */}
-          </div>
-        );
-      })}
-    </div>
+      <h2 className="pieces-heading">Explore Tatoos</h2>
+      <Masonry
+        breakpointCols={breakpointColumnsObj}
+        className="my-masonry-grid"
+        columnClassName="my-masonry-grid_column"
+      >
+        {allPieces}
+      </Masonry>
     </div>
   );
 };
 
 export default Piece;
 
-{/* <div key={piece._id}>
-  <Link to={`/piece/${piece._id}`}>
-    <img src={piece.image} className="piece" alt={piece.title} />
-  </Link>
-</div>; */}
+// <div className="pieces-container">
+// {pieces.map((piece) => {
+//   return (
+//     <div key={piece._id} className="piece-card">
+//       <Link to={`/piece/${piece._id}`}>
+//         <img src={piece.image} alt={piece.title} className="piece-pic" />
+//       </Link>
+//       <div className="piece-info-container">
+//         <h4>
+//           <b></b>
+//           <p className="artist-location"></p>
+
+//         </h4>
+//       </div>
+//     </div>
+//   );
+// })}
+// </div>
