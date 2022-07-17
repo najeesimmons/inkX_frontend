e32774f415e4a88c39180f15dadc3cce2c63030b
najeesimmons <najee.bryant.simmons@gmail.com>
2022-07-16 23:52:46 -0400

    styled Artsits Show, added masonry grid and artist info panel -- I'm proud of this. I'm grateful

--- src/pages/ArtistShow/ArtistShow.js
+++ src/pages/ArtistShow/ArtistShow.js
@@ -1,43 +1,61 @@
 import { useParams } from "react-router";
-import "./artistShow.css"
+import "./artistShow.css";
+import Masonry from "react-masonry-css";
 
+const Show = ({ artists, pieces }) => {
+  const params = useParams();
 
-const Show = (props) => {
-   
-    const params = useParams()
-    
-    const artist = props.artists.find(artist => artist._id === params.id)
-    if (!artist) {
-        return null
-    }
+  const artist = artists.find((artist) => artist._id === params.id);
+  if (!artist) {
+    return null;
+  }
 
-    const pieces = props.pieces.filter(piece => piece.artist === artist._id)
+  const artistPieces = pieces.filter((piece) => piece.artist === artist._id);
 
-    return(
-        <div>
-            <br></br>
-            <div>
-            <img src={artist.profile_pic} className="artist-pic" alt ={artist.username}/>
-            </div>
-            <br></br>
-            <div>
-            <h2>{artist.username}</h2>
-                "{artist.bio}."
-            </div>
-            <br></br>
-            <hr></hr>
-            <br></br>
-            {pieces && <div className="piece-container">
-                {pieces.map(piece => {
-                    return (
-                        <div key={piece._id}>
-                            <img src={piece.image} className="piece" alt={piece.title} />
-                        </div>    
-                    )
-                })} 
-            </div> }
+  const breakpointColumnsObj = {
+    default: 3,
+    1100: 3,
+    700: 2,
+    500: 1,
+  };
+
+  return (
+    <div className="artist-profile-wrapper">
+      <div className="artist-info-container">
+        <img
+          src={artist.profile_pic}
+          className="artist-pic"
+          alt={artist.username}
+        />
+        <h4 className="artist-show-location">{artist.city}, {artist.state}</h4>
+        <h2>{artist.username}</h2>"{artist.bio}."
+        <button>Message</button>
+        <h5>Messaging temporarily disabled.</h5>
+      </div>
+      {artistPieces && (
+        <div className="artist-show-piece-container">
+          <Masonry
+            breakpointCols={breakpointColumnsObj}
+            className="my-masonry-grid"
+            columnClassName="my-masonry-grid_column"
+          >
+            {artistPieces.map((piece) => {
+              return (
+                <div key={piece._id}>
+                  <img
+                    key={piece._id}
+                    src={piece.image}
+                    className="artist-show-profile-piece"
+                    alt={piece.title}
+                  />
+                </div>
+              );
+            })}
+          </Masonry>
         </div>
-    )
+      )}
+    </div>
+  );
 };
 
 export default Show;
