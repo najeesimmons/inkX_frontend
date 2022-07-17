1e25c760d305df928dc01d3e03ebadad6a648f96
najeesimmons <najee.bryant.simmons@gmail.com>
2022-07-17 00:29:34 -0400

    Artist component now sports standardizes photo sizes on artist cards

--- src/pages/Artist/Artist.js
+++ src/pages/Artist/Artist.js
@@ -5,45 +5,33 @@ const Artist = (props) => {
   return (
     <div className="artist-wrapper">
       <h2 className="artist-heading">Find Artists</h2>
-    <div className="artist-container">
-      {props.artists.map((artist) => {
-        return (
-          <div key={artist._id} className="artist-card">
-            <Link to={`/artist/${artist._id}`}>
-              <img
-                src={artist.profile_pic}
-                alt={artist.username}
-                className="artist_pic"
-              />
-            </Link>
-            <div className="artist-info-container">
-              <h4>
-                <b>{artist.username}</b>
-                <p className="artist-location">
-                  {artist.city}, {artist.state}
-                </p>
-              </h4>
+      <div className="artist-container">
+        {props.artists.map((artist) => {
+          return (
+            <div key={artist._id} className="artist-card">
+              <Link to={`/artist/${artist._id}`}>
+                <img
+                  src={artist.profile_pic}
+                  alt={artist.username}
+                  className="artist_pic"
+                />
+              </Link>
+
+              <div className="artist-info-container">
+                <h4>
+                  <b>{artist.username}</b>
+                  <p className="artist-location">
+                    {artist.city}, {artist.state}
+                  </p>
+                </h4>
+              </div>
             </div>
-          </div>
-        );
-      })}
-    </div>
+          );
+        })}
+      </div>
     </div>
   );
 };
 
 export default Artist;
-// <div key={artist._id}>
-//   <br />
-//   <Link to={`/artist/${artist._id}`}>
-//     <img
-//       src={artist.profile_pic}
-//       className="artist_pic"
-//       alt={artist.username}
-//     />
-//     <h3 className="artist-name">{artist.username}</h3>
-//   </Link>
-//   <h3 className="artist-location">
-//     {artist.city}, {artist.state}
-//   </h3>
-// </div>
+
