3b6a5335dedae1fe375466497666aace9e6b84b4
najeesimmons <najee.bryant.simmons@gmail.com>
2022-07-31 02:34:23 -0400

    added brand new styles for main, complete overhaul, funcitonal with queries, modified max-width for nav (expanded)

--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -1,30 +1,42 @@
 import { Link } from "react-router-dom";
+// import { useEffect } from "react";
 import "./main.scss";
-const Main = ({ createArtist, pieces }) => {
+const Main = () => {
+  // useEffect(() => {
+  //   // 👇 add class to body element
+  //   document.body.classList.add("background-image");
+  //   // cleanup
+  //   return () => {
+  //     // 👇️ remove styles when component unmounts
+  //     document.body.classList.remove("background-image");
+  //   };
+  // }, []);
+
   return (
     <>
       <div className="main-container">
-        <div className="tagline-box">
-          {/* <h1 className="inkx">ink x</h1> */}
-          {/* <div className="logo-box">
-          <img src=></img>
-          </div> */}
-          <h1 className="tagline">Want to find the <em>best</em> artist for your next tattoo?</h1>
+        <div className="main-intro-div">
+          <strong>ink X</strong>
+          <h1>Finally, a place for all your tattoo needs.</h1>
+          <div className="hidden-image-div">
+          <img
+            src="https://i.imgur.com/ZDzan4u.png"
+            alt="smiling woman getting a tattoo"
+          />
         </div>
-        <div className="the-process">
-          <h3>We help make it simple:</h3>
           <p>
-            1. Explore our collection to find pieces you love by creators in
-            your area.
+            We know it can be difficult to find the right tattoo artist for you.
+            Our mission is to make it easy for you to find and connect with artists you love.
           </p>
-          <p>
-            2. Learn about the artist -- give em' the vibe check.
-          </p>
-          <p>3. If you like what you find -- connect!</p>
-
-          <button className="explore-button">
-            <Link to="/piece">Explore</Link>
-          </button>
+          <Link to="/piece#">
+            <button className="explore-button">Explore</button>
+          </Link>
+        </div>
+        <div className="main-image-div">
+          <img
+            src="https://i.imgur.com/ZDzan4u.png"
+            alt="smiling woman getting a tattoo"
+          />
         </div>
       </div>
     </>
@@ -32,4 +44,3 @@ const Main = ({ createArtist, pieces }) => {
 };
 
 export default Main;
-
