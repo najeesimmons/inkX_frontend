f06450549dc077b14ab8d4d544919cc884be355d
najeesimmons <najee.bryant.simmons@gmail.com>
2022-07-31 13:34:49 -0400

    changed spacing for button in main for mobile query

--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -19,14 +19,14 @@ const Main = () => {
           <strong>ink X</strong>
           <h1>Finally, a place for all your tattoo needs.</h1>
           <div className="hidden-image-div">
-          <img
-            src="https://i.imgur.com/ZDzan4u.png"
-            alt="smiling woman getting a tattoo"
-          />
-        </div>
+            <img
+              src="https://i.imgur.com/ZDzan4u.png"
+              alt="smiling woman getting a tattoo"
+            />
+          </div>
           <p>
-            We know it can be difficult to find the right tattoo artist for you.
-            Our mission is to make it easy for you to find and connect with artists you love.
+            We know it can be hard to find the right tattoo artist for you. Our
+            mission is to make it easy for you to connect with artists you love.
           </p>
           <Link to="/piece#">
             <button className="explore-button">Explore</button>
