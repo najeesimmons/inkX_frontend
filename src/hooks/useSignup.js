2f09da8807aad52ea6953d2d33b06251e6c4e8b0
najeesimmons <najee.bryant.simmons@gmail.com>
2022-10-17 12:13:50 -0400

    finished authcontext and usesignup hook

--- src/hooks/useSignup.js
+++ src/hooks/useSignup.js
@@ -16,7 +16,7 @@ export const useSignup = () => {
     });
 
     const data = await response.json();
-    console.log(data)
+    console.log(data);
 
     if (!response.ok) {
       setIsLoading(false);
